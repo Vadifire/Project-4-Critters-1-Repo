07e2f1150e9120e757aac335e4049a104e19ebac
Ahsan Khan <ajkhan96@gmail.com>
2016-10-14 21:25:45 -0500

    Restructured file organization

---
+++ src/assignment4/Controller.java
@@ -0,0 +1,53 @@
+package assignment4;
+
+import java.util.Scanner;
+
+public class Controller {
+
+	public boolean quit;
+	Scanner keyboard;
+	
+	public Controller (Scanner kb){
+		quit = false;
+		keyboard = kb;
+	}
+
+	/*
+	 * Prompts the user for a command through Scanner
+	 * Handles the various commands
+	 */
+	public void promptInput(){
+		System.out.print("critters>");
+		String input = keyboard.nextLine();
+		
+		String[] commands = input.split(" ");
+		
+		for (String command : commands){
+			command.trim();
+		}
+		
+		//TODO: handle exceptions
+		
+		switch (commands[0]){
+		case "quit":
+			quit = true;
+			break;
+		case "show":
+			Critter.displayWorld();
+			break;
+			
+		case "step":
+			int count = 1;
+			if (commands.length > 1){
+				count = Integer.parseInt(commands[1]);
+			}
+			while (count > 0){
+				count--;
+				Critter.worldTimeStep();
+			}
+			break;
+		}
+	}
+	
+	
+}
